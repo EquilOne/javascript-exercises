72e6a1a9517d57ce15072121807f1fa396d04e47
Chase Kelly <188378569+EquilOne@users.noreply.github.com>
2025-05-30 16:31:44 -0600

    feat(tempConversion): implement temperature conversion functions
    - implement convertToCelsius to convert fahrenheit to celsius
    - implement convertToFahrenheit to convert celsius to fahrenheit
    - ensure all temperature conversions round to one decimal place
    - enable all temperature conversion tests
    - improve README readability by adding blank lines

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempFahrenheit) {
+  let degreesCelsius = (5 / 9) * (tempFahrenheit - 32);
+  roundDegreesCelsius = Math.round(degreesCelsius * 10) / 10;
+  return roundDegreesCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempCelsius) {
+  let degreesFahrenheit = (9 / 5) * tempCelsius + 32;
+  roundDegreesFahrenheit = Math.round(degreesFahrenheit * 10) / 10;
+  return roundDegreesFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
