68a6753a0918b574d1f2332b1e6af04a370e31d2
Chase Kelly <188378569+EquilOne@users.noreply.github.com>
2025-05-30 13:22:04 -0600

    feat(string): implement reverse string function
    - add core logic for string reversal using split, reverse, and join methods
    - activate previously skipped test cases to validate implementation

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  const charArr = str.split('');
+  const revArr = charArr.reverse();
+  const revStr = revArr.join('');
 
+  return revStr;
 };
+
+console.log(reverseString('backwards'));
 
 // Do not edit below this line
 module.exports = reverseString;
