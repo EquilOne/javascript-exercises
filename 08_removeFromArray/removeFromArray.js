71982212c9c0e0b6027bf4c23ace33e8010f2a77
Chase Kelly <188378569+EquilOne@users.noreply.github.com>
2025-05-30 13:51:15 -0600

    feat(removeFromArray): implement array item removal function
    - add core logic to filter out specified elements from an array
    - enable all tests for the removeFromArray function
    - update string literal quotes in tests

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...toRemove) {
+  const newArray = [];
+  arr.forEach((item) => {
+    if (!toRemove.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
