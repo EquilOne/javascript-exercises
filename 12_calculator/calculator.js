9a8e34648b772e1caaab37853d8576bba2ac4dc4
Chase Kelly <188378569+EquilOne@users.noreply.github.com>
2025-07-06 16:13:08 -0600

    Completed, all tests pass

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,59 @@
-const add = function() {
-	
+const add = function (...numbs) {
+  return numbs.reduce((sum, num) => {
+    sum += num;
+    return sum;
+  }, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function (...numbs) {
+  return numbs.reduce((diff, num) => {
+    diff -= num;
+    return diff;
+  });
 };
 
-const sum = function() {
-	
+const sum = function (...numbs) {
+  if (numbs.length === 0) {
+    return 0;
+  } else {
+    numbs = numbs.flat();
+    return numbs.reduce((sum, num) => {
+      sum += Number(num);
+      return sum;
+    }, 0);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function (...numbs) {
+  numbs = numbs.flat();
+  if (numbs.includes(0)) {
+    return 0;
+  }
+  return numbs.reduce((prod, num) => {
+    prod *= num;
+    return prod;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (...numbs) {
+  numbs = numbs.flat();
+  if (numbs[0] === 0) {
+    return 0;
+  } else if (numbs[1] === 0) {
+    return 1;
+  } else {
+    return numbs[0] ** numbs[1];
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n < 0) {
+    return "Factorial is not defined for negative numbers";
+  }
+  if ((n === 0) | (n === 1)) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +63,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
