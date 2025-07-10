c0b8f36d2ce8f52f2ae98e99c112a83ca1b69e9b
Chase Kelly <188378569+EquilOne@users.noreply.github.com>
2025-07-10 15:08:41 -0600

    Completed, all tests passed.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let cleanedString = str.toLowerCase().replace(/[\W_]/g, "");
+  let reversedString = cleanedString.split("").reverse().join("");
 
+  console.log(cleanedString);
+  console.log(reversedString);
+
+  if (reversedString === cleanedString) {
+    console.log("Palindrome");
+    return true;
+  } else {
+    console.log("Not a palindrome");
+    return false;
+  }
 };
 
+palindromes("r3ace3car");
+
+palindromes("I am a string! With spaces and @special characters...");
 // Do not edit below this line
 module.exports = palindromes;
