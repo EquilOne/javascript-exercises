2cd18ea5d2c5db477a58720129449013d898c586
Chase Kelly <188378569+EquilOne@users.noreply.github.com>
2025-07-10 17:14:37 -0600

    Complete, all tests passed.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
 
+const findTheOldest = function (arr) {
+  const getAge = (person) => {
+    if (!person.yearOfDeath) {
+      return new Date().getFullYear() - person.yearOfBirth;
+    } else {
+      return person.yearOfDeath - person.yearOfBirth;
+    }
+  };
+
+  return arr.reduce((oldest, person) => {
+    if (getAge(person) > getAge(oldest)) return person;
+    else return oldest;
+  });
 };
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
